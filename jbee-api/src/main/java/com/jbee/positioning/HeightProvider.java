ca9dae84da9d6c90516320d1bb41e4fa2fdc1930
Paul Weinhold <weinpau@web.de>
2015-04-28 14:29:54 +0200

    added positioning providers

---
+++ jbee-api/src/main/java/com/jbee/positioning/HeightProvider.java
@@ -0,0 +1,12 @@
+package com.jbee.positioning;
+
+import com.jbee.Provider;
+import com.jbee.units.Distance;
+
+/**
+ *
+ * @author weinpau
+ */
+public interface HeightProvider extends Provider<Distance> {
+
+}
---
+++ jbee-api/src/main/java/com/jbee/positioning/LatLonProvider.java
@@ -0,0 +1,11 @@
+package com.jbee.positioning;
+
+import com.jbee.Provider;
+
+/**
+ *
+ * @author weinpau
+ */
+public interface LatLonProvider extends Provider<LatLon> {
+
+}
