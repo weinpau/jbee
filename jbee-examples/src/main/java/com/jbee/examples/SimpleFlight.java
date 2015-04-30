b0c7fb8600c9f0be5cf01e649bd9967e254d63ff
Paul Weinhold <weinpau@web.de>
2015-04-30 12:09:27 +0200

    updated example

--- jbee-examples/src/main/java/com/jbee/examples/SimpleFlight.java
+++ jbee-examples/src/main/java/com/jbee/examples/SimpleFlight.java
@@ -21,16 +21,17 @@ public class SimpleFlight {
         Bee bee = BeeContext.of(new Simulation()).bootstrap();
 
         BeeControl beeControl = bee.control().
+                onCanceled(c -> System.out.println("command " + c.getCommandNumber() + " canceled")).
                 onPositionChange((c, p) -> {
                     System.out.println("command " + c.getCommandNumber() + ": " + p);
-
                 },
                 Distance.ofCentimeters(50));
 
         beeControl.takeOff();
         beeControl.rotate(YAW.ofDegrees(90));
+        beeControl.up(Distance.ofMeters(5), Velocity.mps(4));
         beeControl.forward(Distance.ofMeters(2), Velocity.mps(1));        
-        beeControl.hover(Duration.ofSeconds(2));
+        beeControl.onAction(c -> beeControl.cancel(), Duration.ofSeconds(1)).hover(Duration.ofSeconds(10));
         beeControl.land();
         
        System.out.println("target position: " + bee.monitor().getLastKnownState().getPosition());
