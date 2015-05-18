172217058ec1eb4b530e962599f6ee49e736a386
Paul Weinhold <weinpau@web.de>
2015-05-18 20:28:25 +0200

    updated OpenLoopControl

--- jbee-device-ardrone2/src/main/java/com/jbee/device/ardrone2/internal/controllers/FlyController.java
+++ jbee-device-ardrone2/src/main/java/com/jbee/device/ardrone2/internal/controllers/FlyController.java
@@ -2,12 +2,17 @@ package com.jbee.device.ardrone2.internal.controllers;
 
 import com.jbee.BeeState;
 import com.jbee.ControlStateMachine;
+import com.jbee.RotationDirection;
 import com.jbee.buses.BeeStateBus;
 import com.jbee.buses.PrincipalAxesBus;
 import com.jbee.commands.CommandResult;
 import com.jbee.commands.FlyCommand;
 import com.jbee.concurrent.CallbackWrapper;
 import com.jbee.device.ardrone2.internal.commands.AT_CommandSender;
+import com.jbee.device.ardrone2.internal.commands.AT_PCMD;
+import com.jbee.positioning.Position;
+import com.jbee.units.Angle;
+import static java.lang.Math.PI;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -46,7 +51,7 @@ public class FlyController implements CommandController<FlyCommand> {
 
         try {
             long timeout = calculateTimeout(command, state);
-            openLoop.control(command);
+            openLoop.control(command, state);
             return commandExecutorService.submit(openLoop).get(timeout, TimeUnit.MILLISECONDS);
         } catch (InterruptedException | ExecutionException | TimeoutException ex) {
             return CommandResult.FAILED;
@@ -67,22 +72,76 @@ public class FlyController implements CommandController<FlyCommand> {
         static final double MIN_DEFLECTION = -1;
         static final double MAX_DEFLECTION = 1;
 
+        static final double EPSILON_HORIZONTAL = .1;
+        static final double EPSILON_ALTITUDE = .1;
+        static final double EPSILON_YAW = 5.0 / 180.0 * PI;
+
+        static final int REQUIRED_ACCEPTABLE_COUNT = 2;
+
         PID xPID = new PID(.5, 0, .35);
         PID yPID = new PID(.5, 0, .35);
         PID zPID = new PID(.8, 0, .35);
         PID yawPID = new PID(1, 0, .3);
 
+        double targetX, targetY, targetZ, targetYAW;
+        int acceptableCount = 0;
+        RotationDirection rotationDirection = RotationDirection.CLOCKWISE;
+
         @Override
         protected void handle() {
-            beeStateBus.subscripe(state -> {    
+            beeStateBus.subscripe(state -> {
+                double yaw = state.getPrincipalAxes().getYaw().toRadians();
 
-               
-                
+                double eX = targetX - state.getPosition().getX();
+                double eY = targetY - state.getPosition().getY();
+                double eZ = targetZ - state.getPosition().getZ();
+                double eYaw = yaw - targetYAW;
+
+                if (acceptable(eX, eY, eZ, eYaw)) {
+
+                    if (++acceptableCount >= REQUIRED_ACCEPTABLE_COUNT) {
+                        submit(CommandResult.COMPLETED);
+                    }
+                }
+
+                double rX = xPID.getRawCommand(eX);
+                double rY = yPID.getRawCommand(eY);
+
+                double dX = deflection(Math.cos(yaw) * rX + Math.sin(yaw) * rY);
+                double dY = deflection(-Math.sin(yaw) * rX + Math.cos(yaw) * rY);
+                double dZ = deflection(zPID.getRawCommand(eZ));
+                double dYaw = deflection(yawPID.getRawCommand(eYaw));
+
+                try {
+                    commandSender.send(new AT_PCMD(false, (float) dX, (float) dY, (float) dZ, (float) dYaw));
+                } catch (InterruptedException e) {
+                }
+
             });
         }
 
-        public void control(FlyCommand command) {
+        boolean acceptable(double eX, double eY, double eZ, double eYaw) {
+            return Math.abs(eX) < EPSILON_HORIZONTAL
+                    && Math.abs(eY) < EPSILON_HORIZONTAL
+                    && Math.abs(eZ) < EPSILON_ALTITUDE
+                    && Math.abs(eYaw) < EPSILON_YAW;
+        }
+
+        public void control(FlyCommand command, BeeState state) {
             reset();
+            configTarget(state, command);
+        }
+
+        void configTarget(BeeState state, FlyCommand command) {
+            Angle initialYAW = state.getPrincipalAxes().getYaw();
+            Position targetPosition = command.calculateTargetPosition(state.getPosition(), initialYAW);
+            targetX = targetPosition.getX();
+            targetY = targetPosition.getY();
+            targetZ = targetPosition.getZ();
+
+            Angle deltaYAW = command.calculateDeltaYAW(initialYAW);
+            targetYAW = initialYAW.add(deltaYAW).normalize().abs().toRadians();
+            rotationDirection = deltaYAW.isNegative() ? RotationDirection.COUNTERCLOCKWISE : RotationDirection.CLOCKWISE;
         }
 
         void reset() {
@@ -90,6 +149,11 @@ public class FlyController implements CommandController<FlyCommand> {
             yPID.reset();
             zPID.reset();
             yawPID.reset();
+            acceptableCount = 0;
+        }
+
+        double deflection(double value) {
+            return Math.max(MIN_DEFLECTION, Math.min(MAX_DEFLECTION, value));
         }
 
     };
@@ -114,7 +178,7 @@ public class FlyController implements CommandController<FlyCommand> {
             errorSum = 0;
         }
 
-        public double getDeflection(double error) {
+        public double getRawCommand(double error) {
 
             long time = System.nanoTime();
             double dt = (time - lastTime) / 10e9d;
