d1cd57e2d37bdae58dbfbb5ba8a60c549286ad74
Paul Weinhold <weinpau@web.de>
2015-05-18 18:10:07 +0200

    updated FlyController

--- jbee-device-ardrone2/src/main/java/com/jbee/device/ardrone2/internal/controllers/FlyController.java
+++ jbee-device-ardrone2/src/main/java/com/jbee/device/ardrone2/internal/controllers/FlyController.java
@@ -1,12 +1,17 @@
 package com.jbee.device.ardrone2.internal.controllers;
 
+import com.jbee.BeeState;
 import com.jbee.ControlStateMachine;
 import com.jbee.buses.BeeStateBus;
 import com.jbee.buses.PrincipalAxesBus;
 import com.jbee.commands.CommandResult;
 import com.jbee.commands.FlyCommand;
+import com.jbee.concurrent.CallbackWrapper;
 import com.jbee.device.ardrone2.internal.commands.AT_CommandSender;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  *
@@ -14,19 +19,12 @@ import java.util.concurrent.ExecutorService;
  */
 public class FlyController implements CommandController<FlyCommand> {
 
-    static final double MIN_DEFLECTION = -1;
-    static final double MAX_DEFLECTION = 1;
-
     AT_CommandSender commandSender;
     BeeStateBus beeStateBus;
     PrincipalAxesBus principalAxesBus;
     ControlStateMachine controlStateMachine;
     ExecutorService commandExecutorService;
-
-    PID xPID = new PID(.5, 0, .35);
-    PID yPID = new PID(.5, 0, .35);
-    PID zPID = new PID(.8, 0, .35);
-    PID yawPID = new PID(1, 0, .3);
+    OpenLoopControl openLoop = new OpenLoopControl();
 
     public FlyController(AT_CommandSender commandSender,
             BeeStateBus beeStateBus,
@@ -40,10 +38,62 @@ public class FlyController implements CommandController<FlyCommand> {
 
     @Override
     public CommandResult execute(FlyCommand command) {
-        return CommandResult.NOT_EXECUTED;
+        if (controlStateMachine.getControlState() != com.jbee.ControlState.FLYING) {
+            return CommandResult.NOT_EXECUTED;
+        }
+
+        BeeState state = beeStateBus.getLastKnownValue().orElse(BeeState.START_STATE);
+
+        try {
+            long timeout = calculateTimeout(command, state);
+            openLoop.control(command);
+            return commandExecutorService.submit(openLoop).get(timeout, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException ex) {
+            return CommandResult.FAILED;
+
+        }
+
     }
 
-   
+    long calculateTimeout(FlyCommand command, BeeState state) {
+        long timeout = 10 + command.calculateDuration(
+                state.getPosition(),
+                state.getPrincipalAxes().getYaw()).toMillis();
+        return timeout * 2;
+    }
+
+    class OpenLoopControl extends CallbackWrapper<CommandResult> {
+
+        static final double MIN_DEFLECTION = -1;
+        static final double MAX_DEFLECTION = 1;
+
+        PID xPID = new PID(.5, 0, .35);
+        PID yPID = new PID(.5, 0, .35);
+        PID zPID = new PID(.8, 0, .35);
+        PID yawPID = new PID(1, 0, .3);
+
+        @Override
+        protected void handle() {
+            beeStateBus.subscripe(state -> {    
+
+               
+                
+            });
+        }
+
+        public void control(FlyCommand command) {
+            reset();
+        }
+
+        void reset() {
+            xPID.reset();
+            yPID.reset();
+            zPID.reset();
+            yawPID.reset();
+        }
+
+    };
+
     static class PID {
 
         private final double kp, ki, kd;
