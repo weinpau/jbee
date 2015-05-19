d676c532406551840668528f1fffc4dc70c3d3df
Paul Weinhold <weinpau@web.de>
2015-05-19 08:30:14 +0200

    updated AngleTest

--- jbee-api/src/test/java/com/jbee/units/AngleTest.java
+++ jbee-api/src/test/java/com/jbee/units/AngleTest.java
@@ -1,5 +1,6 @@
 package com.jbee.units;
 
+import java.time.Duration;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -38,6 +39,78 @@ public class AngleTest {
         assertTrue(Angle.ofDegrees(0).equal(Angle.ofDegrees(360).normalize()));
         assertTrue(Angle.ofDegrees(40).equal(Angle.ofDegrees(400).normalize()));
         assertTrue(Angle.ofDegrees(-40).equal(Angle.ofDegrees(-400).normalize()));
+    }
+
+    @Test
+    public void testAdd() {
+        assertTrue(Angle.ofDegrees(5).add(Angle.ofDegrees(-20)).equal(Angle.ofDegrees(-15)));
+        assertTrue(Angle.ZERO.add(Angle.ZERO).equal(Angle.ZERO));
+    }
+
+    @Test
+    public void testSub() {
+        assertTrue(Angle.ofDegrees(5).sub(Angle.ofDegrees(-20)).equal(Angle.ofDegrees(25)));
+        assertTrue(Angle.ZERO.sub(Angle.ZERO).equal(Angle.ZERO));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertTrue(Angle.ofDegrees(5).multiply(-1).equal(Angle.ofDegrees(-5)));
+       assertTrue(Angle.ofDegrees(5).multiply(0).equal(Angle.ZERO));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMultiply_invalid() {
+        Angle.ofDegrees(5).multiply(Double.NaN);
+      }
+    
+    @Test
+    public void testAbs() {
+    assertTrue(Angle.ofDegrees(-5).abs().equal(Angle.ofDegrees(5)));
+    assertTrue(Angle.ofDegrees(5).abs().equal(Angle.ofDegrees(5)));     
+    assertTrue(Angle.ZERO.abs().equal(Angle.ZERO));     
+      }
+
+    @Test
+    public void testDivide() {
+        AngularSpeed speed = AngularSpeed.ofDegreesPerSecond(5);
+        Angle instance = Angle.ofDegrees(20);
+        Duration expResult = Duration.ofSeconds(4);
+        Duration result = instance.divide(speed);
+        assertEquals(expResult, result);       
+    }
+
+    @Test
+    public void testIsZero() {
+        assertFalse(Angle.ofDegrees(-10).isZero());
+        assertTrue(Angle.ZERO.isZero());
+        assertTrue(Angle.ofDegrees(0).isZero());
+        assertFalse(Angle.ofDegrees(10).isZero());
+    }
+
+    @Test
+    public void testIsNegative() {
+        assertTrue(Angle.ofDegrees(-10).isNegative());
+        assertFalse(Angle.ZERO.isNegative());
+        assertFalse(Angle.ofDegrees(10).isNegative());
+    }
+
+    @Test
+    public void testEqual() {
+        assertTrue(Angle.ofDegrees(10).equal(Angle.ofDegrees(10.000000001)));
+        assertFalse(Angle.ofDegrees(10).equal(Angle.ofDegrees(10.1)));
+    }
+
+    @Test
+    public void testOfRadians() {
+        Angle angle = Angle.ofRadians(1.0);
+        assertEquals(1.0, angle.toRadians(), 0.0001);
+    }
+
+    @Test
+    public void testOfDegrees() {
+        Angle angle = Angle.ofDegrees(5);
+        assertEquals(5.0, angle.toDegrees(), 0.0001);
     }
 
 }
