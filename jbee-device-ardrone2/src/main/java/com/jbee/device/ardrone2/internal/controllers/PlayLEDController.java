080920caec9679dbe36244e377c1a4a01b74a736
Paul Weinhold <weinpau@web.de>
2015-05-19 21:47:13 +0200

    fixed PlayLEDController

--- jbee-device-ardrone2/src/main/java/com/jbee/device/ardrone2/internal/controllers/PlayLEDController.java
+++ jbee-device-ardrone2/src/main/java/com/jbee/device/ardrone2/internal/controllers/PlayLEDController.java
@@ -1,11 +1,9 @@
 package com.jbee.device.ardrone2.internal.controllers;
 
-import com.jbee.ControlState;
 import com.jbee.commands.CommandResult;
 import com.jbee.device.ardrone2.commands.PlayLEDAnimation;
 import com.jbee.device.ardrone2.internal.commands.AT_CommandSender;
 import com.jbee.device.ardrone2.internal.commands.AT_LED;
-import com.jbee.device.ardrone2.internal.commands.AT_PCMD;
 
 /**
  *
@@ -24,7 +22,7 @@ public class PlayLEDController implements CommandController<PlayLEDAnimation> {
         try {
             int animation = command.getAnimation().ordinal();
             float frequency = command.getFrequency().toMilliHz() / 1000f;
-            int duration = StrictMath.toIntExact(command.getDuration().toMillis());
+            int duration = StrictMath.toIntExact(command.getDuration().getSeconds());
             commandSender.send(new AT_LED(animation, frequency, duration));
             return CommandResult.COMPLETED;
         } catch (Exception e) {
