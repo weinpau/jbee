f153a41c518321078ef6300e3a9a1f02d9430394
Paul Weinhold <weinpau@web.de>
2015-05-21 19:36:01 +0200

    updated DefaultPositionBus

--- jbee-core/src/main/java/com/jbee/DefaultPositionBus.java
+++ jbee-core/src/main/java/com/jbee/DefaultPositionBus.java
@@ -13,71 +13,104 @@ import com.jbee.units.Distance;
  * @author weinpau
  */
 final class DefaultPositionBus extends PositionBus {
-
+    
     @Override
     public void bootstrap(TargetDevice device, BusRegistry busRegistry) throws BeeBootstrapException {
-
+        
         LatLon origin = determineOrigin(busRegistry);
-
+        
         if (origin == null) {
-            PositionEstimator positionEstimator = new PositionEstimator();
-
+            VelocityPositionEstimator positionEstimator = new VelocityPositionEstimator();
+            
             busRegistry.get(VelocityBus.class).
                     orElseThrow(() -> new BeeBootstrapException("A translational velocity bus is missing.")).
                     subscripe(v -> {
-
+                        
                         publish(positionEstimator.estimatePosition(v));
                     });
-
+            
             busRegistry.get(AltitudeBus.class).
                     ifPresent(b -> b.subscripe(d -> {
                         publish(positionEstimator.estimatePosition(d));
                     }));
         } else {
-
+            
+            LatLonPositionEstimator positionEstimator = new LatLonPositionEstimator();
+            
             busRegistry.get(LatLonBus.class).
                     orElseThrow(() -> new BeeBootstrapException("A GPS bus is missing.")).
                     subscripe(geo -> {
-                        publish(geo.toPosition(origin));
+                        publish(positionEstimator.estimatePosition(origin, geo));
                     });
+            
+            busRegistry.get(AltitudeBus.class).
+                    ifPresent(b -> b.subscripe(d -> {
+                        publish(positionEstimator.estimatePosition(origin, d));
+                    }));
         }
     }
-
-    static class PositionEstimator {
-
+    
+    static class LatLonPositionEstimator {
+        
+        LatLon geo;
+        Distance altitude;
+        
+        Position estimatePosition(LatLon origin, Distance altitude) {
+            this.altitude = altitude;
+            return createPosition(origin);
+        }
+        
+        Position estimatePosition(LatLon origin, LatLon geo) {
+            this.geo = geo;
+            return createPosition(origin);
+        }
+        
+        private Position createPosition(LatLon origin) {
+            if (geo == null && altitude != null) {
+                return Position.ORIGIN.withZ(altitude.toMeters());
+            } else if (geo != null && altitude == null) {
+                return geo.toPosition(origin);
+            } else {
+                return geo.toPosition(origin).withZ(altitude.toMeters());
+            }
+        }        
+    }
+    
+    static class VelocityPositionEstimator {
+        
         static final long NANOS_PER_SECOND = 1000_000_000L;
-
+        
         long lastTime;
         double x, y, z;
-
+        
         Position estimatePosition(Distance altitude) {
             z = altitude.toMeters();
             return new Position(x, y, z);
         }
-
+        
         Position estimatePosition(Velocity velocity) {
             long now = System.nanoTime();
             if (lastTime == 0) {
                 lastTime = now;
                 return Position.ORIGIN;
             }
-
+            
             double dt = (now - lastTime) / (double) NANOS_PER_SECOND;
             double vx = velocity.getX().mps();
             double vy = velocity.getY().mps();
             double vz = velocity.getZ().mps();
-
+            
             x += dt * vx;
             y += dt * vy;
             z += dt * vz;
             lastTime = now;
-
+            
             return new Position(x, y, z);
         }
     }
-
+    
     private LatLon determineOrigin(BusRegistry busRegistry) throws BeeBootstrapException {
-
+        
         OriginDeterminer originDeterminer = new OriginDeterminer(busRegistry);
         if (originDeterminer.isDeterminable()) {
             try {
@@ -89,5 +122,5 @@ final class DefaultPositionBus extends PositionBus {
             return null;
         }
     }
-
+    
 }
